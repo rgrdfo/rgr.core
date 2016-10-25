268610b74840033d3a14614ca036c65cce4a301f
Михаил Черниенко <eastwing27@gmail.com>
2016-10-25 17:46:58 +1000

    Начата работа над сортером результатов поиска

---
+++ src/RGR.Core/ViewComponents/SearchResultSorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using RGR.Core.Controllers;
+
+namespace RGR.Core.ViewComponents
+{
+    public class SearchResultSorter : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(IEnumerable<ObjectPassport> Result, Func<ObjectPassport, string> SortingOrder)
+        {
+            return await Task.Run(() => View(Result.OrderBy(SortingOrder)));
+        }
+    }
+}
