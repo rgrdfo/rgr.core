e84e97780bedde639af6abf100b58244256f7f1a
Ann Oleynik <oleynik.ann96@mail.ru>
2016-11-14 15:09:55 +1000

    Реализован вывод объектов в компонент представления Account

--- src/RGR.Core/Views/Shared/Components/PersonalObjects/PersonalObjects.cs
+++ src/RGR.Core/Views/Shared/Components/PersonalObjects/PersonalObjects.cs
@@ -18,105 +18,108 @@ namespace RGR.Core.Views.Shared.Components
 
         public async Task<IViewComponentResult> InvokeAsync(IEnumerable<ShortPassport> Source)
         {
-            var grouped = Source.GroupBy(l => l["Type"]).Select(l => l.ToList());            
+            var grouped1 = Source.GroupBy(l => l["Type"]);
+            var grouped = grouped1.Select(l => l.ToList());
+
             return View(await Task.Run(() => Render(grouped)));
         }
 
 
         // Вкладка Квартира (id=4)
-        public static HtmlString StringBuilderFlat(List<ShortPassport> flats)
+        public static string StringBuilderFlat(List<ShortPassport> flats)
         {
-            StringBuilder sb = new StringBuilder("<div id = \"typeObj4\" class=\"tab - pane \">");
+            var sb = new StringBuilder("<div id = \"typeObj4\" class=\"tab-pane \">");
             if (flats != null)
             {
-                sb.Append($"< h5 >< b > Новых совпадений по объектам:{(flats != null ? flats.Count() : 0)} </ b ></ h5 >");
+                sb.Append($"<h5><b> Новых совпадений по объектам:{(flats != null ? flats.Count() : 0)} </b ></h5>");
                 foreach (var flat in flats)
                 {
-                    sb.Append($"<div class=\"search-box-head\">");
-                    sb.Append($"<div class=\"row\"><div class=\"col-lg-3\"><a href=\"#\">Адрес</a></div>");
+                    sb.Append($"<div class=\"object-conteiner\"><div class=\"account-box-head\">");
+                    sb.Append($"<div class=\"row\"><div class=\"col-lg-3\">Адрес</div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Дата</label></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Цена</label></div>");
                     sb.Append($"<div class=\"col-lg-2\"><label>Комнаты</label></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Площадь</label></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Этажность</label></div>");
-                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div>");                    
+                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div></div>");                    
 
-                    sb.Append($"< div class=\"row\"><div class=\"col-lg-3\" style=\"border-bottom:none;\"><h1><span>{flat["Address"]}<br/>{flat["City"]}</span></h1>");
-                    sb.Append($"< img src =\"{flat["Logo"]}\"></div>");
-                    sb.Append($"<div class=\"col-lg-1\"><h1>{flat["DateToShow"]}</h1><br />ID: {flat["Id"]:0000000}</div>");
+                    sb.Append($"<div class=\"row\"><div class=\"col-lg-3\" style=\"border-bottom:none;\"><h1>{flat["Address"]}<br/>{flat["City"]}</h1></div>");
+                    //sb.Append($"< img src =\"{flat["Logo"]}\"></div>");
+                    sb.Append($"<div class=\"col-lg-1\"><h1>{flat["Date"]}</h1><br />ID: {flat["Id"]:0000000}</div>");
                     sb.Append($"<div class=\"col-lg-1\"><h1>{flat["Price"]: ### ### ###} ₽</h1><br />{flat["PricePerSquare"]:### ###.##} ₽ / м²</div>");
                     sb.Append($"<div class=\"col-lg-2\"><h1>{flat["Rooms"]}-комнатная</h1><br/>{flat["HouseMaterial"]}<br/>{flat["HouseType"]}</div>");
-                    sb.Append($"<div class=\"col-lg-1\"><h1>{flat["Area"]} м²</h1><br />кухня {flat["KitchenArea"]}<br />жилая {flat["LivingArea"]}</div>");
+                    sb.Append($"<div class=\"col-lg-1\"><h1>{flat["Area"]} м²</h1><br />кухня: {flat["KitchenArea"]} м²<br />жилая: {flat["LivingArea"]} м²</div>");
                     sb.Append($"<div class=\"col-lg-1\"><h1>{flat["Floor"]} этаж из {flat["FloorCount"]}</h1><br />{flat["Description"]}</div>");
                     sb.Append($"<div class=\"col-lg-2\">{flat["Agency"]}<br/>{flat["AgentPhone"]}</div></div>");
+                    sb.Append($"<div class=\"search-result-footer\"><a href=\"Info?id={flat["Id"]}\">Подробнее</a></div></div>");
                 }
-                sb.Append($"</div></div>");
-                return new HtmlString(sb.ToString());
+                sb.Append($"</div>");
+                return sb.ToString();
             }
             else
             {
-                sb.Append($"</div>");
-                return new HtmlString(sb.ToString());
+                sb.Append($"<h4>У вас пока нет не одного объекта!</h4></div>");
+                return sb.ToString();
             }
         }
 
         // Вкладка Дом (id=1)
-        public static HtmlString StringBuilderHouse(List<ShortPassport> houses)
+        public static string StringBuilderHouse(List<ShortPassport> houses)
         {
-            StringBuilder sb = new StringBuilder("<div id = \"typeObj1\" class=\"tab - pane in active\">");
+            var sb = new StringBuilder("<div id = \"typeObj1\" class=\"tab-pane in active\">");
             if (houses != null)
             {
-                sb.Append($"<div class=\"search-box-head\">");
                 sb.Append($"< h5 >< b > Новых совпадений по объектам:{(houses != null ? houses.Count() : 0)} </ b ></ h5 >");
                 foreach (var house in houses)
                 {
+                    sb.Append($"<div class=\"object-conteiner\"><div class=\"search-box-head\">");
                     sb.Append($"<div class=\"row\"><div class=\"col-lg-3\"><a href=\"#\">Адрес</a></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Дата</label></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Цена</label></div>");
                     sb.Append($"<div class=\"col-lg-2\"><label>Комнаты</label></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Площадь</label></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Этажность</label></div>");
-                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div>");                    
+                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div></div>");                    
 
-                    sb.Append($"< div class=\"row\"><div class=\"col-lg-3\" style=\"border-bottom:none;\"><h1><span>{house["Address"]}<br/>{house["City"]}</span></h1>");
-                    sb.Append($"< img src =\"{house["Logo"]}\"></div>");
+                    sb.Append($"< div class=\"row\"><div class=\"col-lg-3\" style=\"border-bottom:none;\"><h1><span>{house["Address"]}<br/>{house["City"]}</span></h1></div>");
+                    //sb.Append($"< img src =\"{house["Logo"]}\"></div>");
                     sb.Append($"<div class=\"col-lg-1\"><h1>{house["DateToShow"]}</h1><br />ID: {house["Id"]:0000000}</div>");
                     sb.Append($"<div class=\"col-lg-1\"><h1>{house["Price"]: ### ### ###} ₽</h1><br />{house["PricePerSquare"]:### ###.##} ₽ / м²</div>");
                     sb.Append($"<div class=\"col-lg-2\">{house["Rooms"]}-комн.<br />материал: {house["HouseMaterial"]}<br/>состояние: {house["State"]}<br/>отопление: {house["Heating"]}<br/>вода: {house["Water"]}<br/>электричество: {house["Electricy"]}<br/>канализация: {house["Sewer"]}</div>");
                     sb.Append($"<div class=\"col-lg-1\">{house["Area"]:###.##} м²<br />кухня: {house["KitchenArea"]} м²<br/>жилая: {house["LivingArea"]} м²</div>");
                     sb.Append($"<div class=\"col-lg-1\">{house["FloorCount"]}<br />балкон: {house["BalconyIsPresent"]}<br/>санузел: {house["WC"]}</div>");
                     sb.Append($"<div class=\"col-lg-2\">{house["Agency"]}<br/>{house["AgentPhone"]}</div></div>");
+                    sb.Append($"<div class=\"search-result-footer\"><a href=\"Info?id={house["Id"]}\">Подробнее</a></div></div>");
                 }
-                sb.Append($"</div></div>");
-                return new HtmlString(sb.ToString());             
+                sb.Append($"</div>");
+                return sb.ToString();
 
             }
             else
             {
-                sb.Append($"</div>");
-                return new HtmlString(sb.ToString());
+                sb.Append($"<h4>У вас пока нет не одного объекта!</h4></div>");
+                return sb.ToString();
             }
             
 
         }
 
         // Вкладка Гараж (id=3)
-        public static HtmlString StringBuilderGarage(List<ShortPassport> garages)
+        public static string StringBuilderGarage(List<ShortPassport> garages)
         {
-            StringBuilder sb = new StringBuilder("<div id = \"typeObj3\" class=\"tab - pane\">");
+            var sb = new StringBuilder("<div id = \"typeObj3\" class=\"tab-pane\">");
             if (garages != null)
             {
-                sb.Append($"< div class=\"tab - content\"><div id = \"typeObj3\" class=\"tab - pane\">");
                 sb.Append($"< h5 >< b > Новых совпадений по объектам:{(garages != null ? garages.Count() : 0)} </ b ></ h5 >");
                 foreach (var garage in garages)
                 {
-                    sb.Append($"<div class=\"search-box-head\">");
+                    sb.Append($"<div class=\"object-conteiner\"><div class=\"account-box-head\">");
                     sb.Append($"<div class=\"row\"><div class=\"col-lg-3\"><a href=\"#\">Адрес</a></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Дата</label></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Цена</label></div>");
                     sb.Append($"<div class=\"col-lg-3\"><label>Тип</label></div>");
                     sb.Append($"<div class=\"col-lg-3\"><label>Площадь</label></div>");
-                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div>");
+                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div></div>");
                     
                     sb.Append($"< div class=\"row\"><div class=\"col-lg-3\" style=\"border-bottom:none;\"><h1><span>{garage["Address"]}<br/>{garage["City"]}</span></h1>");
                     sb.Append($"< img src =\"{garage["Logo"]}\"></div>");
@@ -125,34 +128,35 @@ namespace RGR.Core.Views.Shared.Components
                     sb.Append($"<div class=\"col-lg-3\">Гараж<br />материал: {garage["garageMaterial"]}</div>");
                     sb.Append($"<div class=\"col-lg-3\">{garage["Area"]:###.##} м²</div>");
                     sb.Append($"<div class=\"col-lg-2\">{garage["Agency"]}<br/>{garage["AgentPhone"]}</div></div>");
+                    sb.Append($"<div class=\"search-result-footer\"><a href=\"Info?id={garage["Id"]}\">Подробнее</a></div></div>");
+
                 }
-                sb.Append($"</div></div>");
-                return new HtmlString(sb.ToString());
+                sb.Append($"</div>");
+                return sb.ToString();
             }
             else
             {
-                sb.Append($"</div>");
-                return new HtmlString(sb.ToString());
+                sb.Append($"<h4>У вас пока нет не одного объекта!</h4></div>");
+                return sb.ToString();
             }
         }
 
         // Вкладка Участок (id=2)
-        public static HtmlString StringBuilderLand(List<ShortPassport> lands)
+        public static string StringBuilderLand(List<ShortPassport> lands)
         {
-            StringBuilder sb = new StringBuilder("<div id = \"typeObj2\" class=\"tab - pane\">");
+            var sb = new StringBuilder("<div id = \"typeObj2\" class=\"tab-pane\">");
             if (lands != null)
             {
-                sb.Append($"< div class=\"tab - content\"><div id = \"typeObj4\" class=\"tab - pane\">");
                 sb.Append($"< h5 >< b > Новых совпадений по объектам:{(lands != null ? lands.Count() : 0)} </ b ></ h5 >");
                 foreach (var land in lands)
                 {
-                    sb.Append($"<div class=\"search-box-head\">");
+                    sb.Append($"<div class=\"object-conteiner\"><div class=\"account-box-head\">");
                     sb.Append($"<div class=\"row\"><div class=\"col-lg-3\"><a href=\"#\">Адрес</a></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Дата</label></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Цена</label></div>");
                     sb.Append($"<div class=\"col-lg-3\"><label>Площадь</label></div>");
                     sb.Append($"<div class=\"col-lg-3\"><label>Этажей</label></div>");
-                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div>");
+                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div></div>");
 
                     sb.Append($"< div class=\"row\"><div class=\"col-lg-3\" style=\"border-bottom:none;\"><h1><span>{land["Address"]}<br/>{land["City"]}</span></h1>");
                     sb.Append($"< img src =\"{land["Logo"]}\"></div>");
@@ -161,34 +165,34 @@ namespace RGR.Core.Views.Shared.Components
                     sb.Append($"<div class=\"col-lg-3\">{land["Area"]:###.##} м²<br /><br/>отопление: {land["Heating"]}<br/>вода: {land["Water"]}<br/>электричество: {land["Electricy"]}<br/>канализация: {land["Sewer"]}</div>");
                     sb.Append($"<div class=\"col-lg-3\">{land["Purpose"]}<br />{land["Category"]}<br />{land["Specifics"]}</div>");
                     sb.Append($"<div class=\"col-lg-2\">{land["Agency"]}<br/>{land["AgentPhone"]}</div></div>");
+                    sb.Append($"<div class=\"search-result-footer\"><a href=\"Info?id={land["Id"]}\">Подробнее</a></div></div>");
                 }
-                sb.Append($"</div></div>");
-                return new HtmlString(sb.ToString());
+                sb.Append($"</div>");
+                return sb.ToString();
             }
             else
             {
-                sb.Append($"</div>");
-                return new HtmlString(sb.ToString());
+                sb.Append($"<h4>У вас пока нет не одного объекта!</h4></div>");
+                return sb.ToString();
             }
         }
 
         // Вкладка Для бизнеса (id=5)
-        public static HtmlString StringBuilderOffice(List<ShortPassport> offices)
+        public static string StringBuilderOffice(List<ShortPassport> offices)
         {
-            StringBuilder sb = new StringBuilder("<div id = \"typeObj5\" class=\"tab - pane\">");
+            var sb = new StringBuilder("<div id = \"typeObj5\" class=\"tab-pane\">");
             if (offices != null)
             {
-                sb.Append($"< div class=\"tab - content\"><div id = \"typeObj4\" class=\"tab - pane\">");
                 sb.Append($"< h5 >< b > Новых совпадений по объектам:{(offices != null ? offices.Count() : 0)} </ b ></ h5 >");
                 foreach (var office in offices)
                 {
-                    sb.Append($"<div class=\"search-box-head\">");
+                    sb.Append($"<div class=\"object-conteiner\"><div class=\"account-box-head\">");
                     sb.Append($"<div class=\"row\"><div class=\"col-lg-3\"><a href=\"#\">Адрес</a></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Дата</label></div>");
                     sb.Append($"<div class=\"col-lg-1\"><label>Цена</label></div>");
                     sb.Append($"<div class=\"col-lg-3\"><label>Площадь</label></div>");
                     sb.Append($"<div class=\"col-lg-3\"><label>Этажей</label></div>");
-                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div>");
+                    sb.Append($"<div class=\"col-lg-2\"><label></br></label></div></div></div>");
 
                     sb.Append($"< div class=\"row\"><div class=\"col-lg-3\" style=\"border-bottom:none;\"><h1><span>{office["Address"]}<br/>{office["City"]}</span></h1>");
                     sb.Append($"< img src =\"{office["Logo"]}\"></div>");
@@ -197,14 +201,15 @@ namespace RGR.Core.Views.Shared.Components
                     sb.Append($"<div class=\"col-lg-3\">{office["Area"]:###.##} м²<br />материал: {office["HouseMaterial"]}<br />состояние: {office["State"]}</div>");
                     sb.Append($"<div class=\"col-lg-3\">{office["FloorCount"]}<br /><br/>отопление: {office["Heating"]}<br/>вода: {office["Water"]}<br/>электричество: {office["Electricy"]}<br/>канализация: {office["Sewer"]}</div>");
                     sb.Append($"<div class=\"col-lg-2\">{office["Agency"]}<br/>{office["AgentPhone"]}</div></div>");
+                    sb.Append($"<div class=\"search-result-footer\"><a href=\"Info?id={office["Id"]}\">Подробнее</a></div></div>");
                 }
-                sb.Append($"</div></div>");
-                return new HtmlString(sb.ToString());
+                sb.Append($"</div>");
+                return sb.ToString();
             }
             else
             {
-                sb.Append($"</div>");
-                return new HtmlString(sb.ToString());
+                sb.Append($"<h4>У вас пока нет не одного объекта!</h4></div>");
+                return sb.ToString(); ;
             }
         }
 
@@ -220,16 +225,16 @@ namespace RGR.Core.Views.Shared.Components
             else
             {
                 var sb = new StringBuilder("<div id=\"bodyResult\"><div class=\"tab-content\">");
-                var flats = grouped.FirstOrDefault(g => g.Any(f => f["Type"].Equals(EstateTypes.Flat)));
-                var houses = grouped.FirstOrDefault(g => g.Any(f => f["Type"].Equals(EstateTypes.House)));
-                var lands = grouped.FirstOrDefault(g => g.Any(f => f["Type"].Equals(EstateTypes.Land)));
-                var garages = grouped.FirstOrDefault(g => g.Any(f => f["Type"].Equals(EstateTypes.Garage)));
-                var offices = grouped.FirstOrDefault(g => g.Any(f => f["Type"].Equals(EstateTypes.Office)));
-                StringBuilderHouse(houses);
-                StringBuilderLand(lands);
-                StringBuilderGarage(garages);
-                StringBuilderFlat(flats);
-                StringBuilderOffice(offices);
+                var flats = grouped.FirstOrDefault(g => g.Any(f =>   (long)f["Type"] == (long)EstateTypes.Flat));
+                var houses = grouped.FirstOrDefault(g => g.Any(f =>  (long)f["Type"] == (long)EstateTypes.House));
+                var lands = grouped.FirstOrDefault(g => g.Any(f =>   (long)f["Type"] == (long)EstateTypes.Land));
+                var garages = grouped.FirstOrDefault(g => g.Any(f => (long)f["Type"] == (long)EstateTypes.Garage));
+                var offices = grouped.FirstOrDefault(g => g.Any(f => (long)f["Type"] == (long)EstateTypes.Office));
+                sb.Append(StringBuilderHouse(houses));
+                sb.Append(StringBuilderLand(lands));
+                sb.Append(StringBuilderGarage(garages));
+                sb.Append(StringBuilderFlat(flats));
+                sb.Append(StringBuilderOffice(offices));
                 sb.Append($"</div></div>");               
                 return new HtmlString(sb.ToString());
             }
