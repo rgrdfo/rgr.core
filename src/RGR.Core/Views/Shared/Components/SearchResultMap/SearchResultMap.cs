8576d6e16e2788e4cbb68291e1f3de690fc831b7
Ann Oleynik <oleynik.ann96@mail.ru>
2016-11-30 17:56:43 +1000

    Реализованы вкладки в результатах поиска. Карта!!!!

---
+++ src/RGR.Core/Views/Shared/Components/SearchResultMap/SearchResultMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RGR.Core.Common;
+using RGR.Core.Common.Enums;
+
+namespace RGR.Core.ViewComponents
+{
+    public class SearchResultMap : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(IEnumerable<ShortPassport> Result, string OrderingField)
+        {
+            //ViewData["Type"] = Type;
+            return await Task.Run(() => View(Result.OrderBy(estate => estate[OrderingField])));
+        }
+    }
+}
